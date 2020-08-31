add1e2ff4b12f7f7a34fedf6abb4571a789afea9
Yeap Chun Lik <yeapchunlik@gmail.com>
2020-09-01 03:24:08 +0800

    added Level-3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
-
-
 
 public class Duke {
 
@@ -16,8 +13,6 @@ public class Duke {
         System.out.println("What can I do for you?");
     }
 
-
-
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,7 +22,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         printGreet();
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Task> list = new ArrayList<>();
         String userInput;
 
         while(true) {
@@ -41,12 +36,25 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 printHorizontalLine();
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(i+1 + ". " + list.get(i));
+                    System.out.println( (i+1) + ".[" + list.get(i).getStatusIcon() + "] " + list.get(i).getDescription());
                 }
                 printHorizontalLine();
+            } else if (userInput.startsWith("done")) {
+                int index = Integer.parseInt(userInput.substring(5));
+                if (index < 0 || index > list.size()) {
+                    System.out.println("Oops, index not found! Try again.");
+                    continue;
+                }
+                list.get(index - 1).markAsDone();
+                printHorizontalLine();
+                System.out.println("Nice!  I've marked this task as done:");
+                System.out.println("[" + list.get(index - 1).getStatusIcon() + "] " + list.get(index - 1).getDescription());
+                printHorizontalLine();
             } else {
-                list.add(userInput);
+                Task t = new Task(userInput);
+                list.add(t);
                 printHorizontalLine();
                 System.out.println("added: " + userInput);
                 printHorizontalLine();
