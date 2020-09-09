473c594edad9c5c4adc76e86153547d2f0eaeda8
Yeap Chun Lik <yeapchunlik@gmail.com>
2020-09-09 19:42:06 +0800

    added InputParser class and DukeException for handling errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int MAX_TASKS = 100;
-    private static final String horizontalLine = "____________________________________________________________\n";
+    public static final String horizontalLine = "____________________________________________________________\n";
     private static final Task[] tasks = new Task[MAX_TASKS];
 //    private static String[] parsedInput = new String[2];
 
@@ -27,14 +27,12 @@ public class Duke {
 //    }
 
     private static void execute() throws DukeException {
-        Scanner in = new Scanner(System.in);
 
         while(true) {
+            Scanner in = new Scanner(System.in);
             String userInput = in.nextLine().trim();
             InputParser taskCommand = new InputParser(userInput);
-            taskCommand.analyseTask();
-            InputParser taskDescription = new InputParser(taskCommand.strings[1]);
-//            String[] userInput = in.nextLine().trim().split(" ",2);
+
             switch (taskCommand.strings[0]) {
                 case "bye":
                     String farewellMessage = "Bye. Hope to see you again soon!";
@@ -42,42 +40,35 @@ public class Duke {
                     return;
                 // Fallthrough
                 case "todo":
-//                        checkInputValidity(userInput[1]);
                     tasks[ToDo.taskNumber] = new ToDo(taskCommand.strings[1]);
                     ToDo.addTask(tasks[ToDo.taskNumber]);
                     break;
                 case "deadline":
-//                    String[] deadlineDetail = userInput[1].split("/", 2);
-//                    checkInputValidity(deadlineDetail[1]);
-                    taskDescription.analyseTaskDescription();
-                    tasks[Deadline.taskNumber] = new Deadline(taskDescription.strings[0], taskDescription.strings[1].substring(3));
+                    taskCommand.analyseTaskDescription();
+                    tasks[Deadline.taskNumber] = new Deadline(taskCommand.strings[0], taskCommand.strings[1].substring(3));
                     Deadline.addTask(tasks[Deadline.taskNumber]);
                     break;
                 case "event":
-//                    String[] eventDetail = userInput[1].split("/", 2);
-                    taskDescription.analyseTaskDescription();
-                    tasks[Event.taskNumber] = new Event(taskDescription.strings[0], taskDescription.strings[1].substring(3));
+                    taskCommand.analyseTaskDescription();
+                    tasks[Event.taskNumber] = new Event(taskCommand.strings[0], taskCommand.strings[1].substring(3));
                     Event.addTask((tasks[Event.taskNumber]));
                     break;
                 case "list":
                     System.out.println(horizontalLine + "Here are the tasks in your list: ");
-//                    for (int i = 0; i < Task.taskNumber; i++) {
-//                        System.out.println((i + 1) + "." + tasks[i]);
-//                    }
-//                    System.out.println(horizontalLine);
-//                    System.out.println(tasks[0]);
-//                    System.out.println(tasks[1]);
+                    for (int i = 0; i < Task.taskNumber; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+                    System.out.println(horizontalLine);
+
                     break;
                 case "done":
-//                    checkInputValidity(userInput[1]);
                     int doneIndex = Integer.parseInt(taskCommand.strings[1]);
                     tasks[doneIndex - 1].markAsDone();
                     System.out.println(horizontalLine + "Nice!  I've marked this task as done:");
                     System.out.println("[" + tasks[doneIndex - 1].getStatusIcon() + "] " + tasks[doneIndex - 1].getDescription() + "\n" + horizontalLine);
                     break;
                 default:
-                    throw new DukeException(("\n" + horizontalLine + "Sorry I don't get your command.\nTips: please use " +
-                            "keywords such as 'todo', 'deadline' or 'event'\n" + horizontalLine));
+                    throw new DukeException(DukeException.ExceptionType.INVALID_INPUT);
             }
 
         }
